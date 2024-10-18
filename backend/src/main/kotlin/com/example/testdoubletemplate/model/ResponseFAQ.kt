f081d0715650e07344a7881bf734e9a4d40c9928
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-10-19 00:47:59 +0900

    FAQのレスポンス用の型を定義

---
+++ backend/src/main/kotlin/com/example/testdoubletemplate/model/ResponseFAQ.kt
@@ -0,0 +1,11 @@
+package com.example.testdoubletemplate.model
+
+data class ResponseFAQ(
+    val category: String,
+    val questionAndAnswerList: List<QuestionAndAnswer>
+)
+
+data class QuestionAndAnswer(
+    val question: String,
+    val answer: String
+)
