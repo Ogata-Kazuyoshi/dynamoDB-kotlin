e2d25db9ee31588fd36382454cf5f0f8c6b47b1d
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-10-19 11:48:49 +0900

    TableConfigの設定方法を変えて、Beanの登録は１つのメソッドでやる仕様に変更

--- backend/src/main/kotlin/com/example/testdoubletemplate/config/DynamoDBInformationTableConfig.kt
+++ backend/src/main/kotlin/com/example/testdoubletemplate/config/DynamoDBInformationTableConfig.kt
@@ -8,30 +8,21 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema
 
 @Configuration
 class DynamoDBInformationTableConfig {
     @Bean
+    @Qualifier("dynamoDBTableForInformation")
     fun dynamoDBRepositoryForInformation(
         dynamoDbClient: DynamoDbEnhancedClient,
-        @Qualifier("dynamoDBTableForInformation")
-        dynamoDBTable: DynamoDbTable<InformationTableEntity>
-    ): NoSQLRepository<InformationTableEntity> {
-        return DynamoDBRepository(dynamoDBTable)
-    }
-
-    @Bean
-    fun dynamoDBTableForInformation(
-        dynamoDBClient: DynamoDbEnhancedClient,
         @Value("\${spring.profiles.active}")
         environment: String
-    ): DynamoDbTable<InformationTableEntity> {
-        return dynamoDBClient
-            .table(
-                "information_table_${environment}",
-                TableSchema.fromBean(InformationTableEntity::class.java)
-            )
+    ): NoSQLRepository<InformationTableEntity> {
+        val dynamoDBTable = dynamoDbClient.table(
+            "information_table_${environment}",
+            TableSchema.fromBean(InformationTableEntity::class.java)
+        )
+        return DynamoDBRepository(dynamoDBTable)
     }
 }
